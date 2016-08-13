fa738cf3ba2022f684cbe9e0f50836548d7b20ac
Alik Elzin <kilaka@gmail.com>
2016-08-13 07:35:54 +0300

    Dish activity - legend and explanation.

--- app/src/main/java/happy_panda/client/DishActivity.java
+++ app/src/main/java/happy_panda/client/DishActivity.java
@@ -15,6 +15,7 @@ import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.io.IOException;
@@ -61,14 +62,15 @@ public class DishActivity extends AppCompatActivity {
 
 		List<BarEntry> entries = new ArrayList<>();
 		entries.add(new BarEntry(0f, 30f));
-		entries.add(new BarEntry(1f, 80f));
-		entries.add(new BarEntry(2f, 60f));
-		entries.add(new BarEntry(3f, 50f));
+		entries.add(new BarEntry(1f, 40f));
+		entries.add(new BarEntry(2f, 50f));
+		entries.add(new BarEntry(3f, 40f));
 		// gap of 2f
-		entries.add(new BarEntry(5f, 70f));
-		entries.add(new BarEntry(6f, 60f));
+		entries.add(new BarEntry(4f, 55f));
+		entries.add(new BarEntry(5f, 45f));
 
 		BarDataSet set = new BarDataSet(entries, "BarDataSet");
+//		set.set
 		/*int[] a = new int[ColorTemplate.VORDIPLOM_COLORS.length + ColorTemplate.LIBERTY_COLORS.length];
 		int i;
 		for(i = 0; i < ColorTemplate.VORDIPLOM_COLORS.length; i++ )
@@ -82,22 +84,29 @@ public class DishActivity extends AppCompatActivity {
 			i++;
 		}*/
 
-		set.setColors(ColorTemplate.VORDIPLOM_COLORS);
+		int[] colorTemplate = {
+				Color.rgb(64, 89, 128), Color.rgb(149, 165, 124), Color.rgb(217, 184, 162),
+				Color.rgb(191, 134, 134), Color.rgb(179, 48, 80), Color.rgb(250, 100, 100)
+		};
+		set.setColors(colorTemplate);
+//		set.setStackLabels(new String[] {"aa", "bb", "cc", "dd", "e", "f"});
+//		new BarDataSet("aa", "bb", "cc", "dd");
 		BarData data = new BarData(set);
 		data.setBarWidth(0.9f); // set custom bar width
 		chart.setData(data);
 		chart.setFitBars(true); // make the x-axis fit exactly all bars
 		Legend l = chart.getLegend();
-		l.setFormSize(10f); // set the size of the legend forms/shapes
-		l.setForm(Legend.LegendForm.CIRCLE); // set what type of form/shape should be used
-		l.setPosition(Legend.LegendPosition.BELOW_CHART_LEFT);
+//		l.setDrawInside(true);
+//		l.setFormSize(10f); // set the size of the legend forms/shapes
+		l.setForm(Legend.LegendForm.SQUARE); // set what type of form/shape should be used
+		l.setPosition(Legend.LegendPosition.BELOW_CHART_CENTER);
 		l.setTextSize(12f);
 		l.setTextColor(Color.BLACK);
 		l.setXEntrySpace(5f); // set the space between the legend entries on the x-axis
 		l.setYEntrySpace(5f); // set the space between the legend entries on the y-axis
-
-		// set custom labels and colors
-		l.setCustom(ColorTemplate.VORDIPLOM_COLORS, new String[] { "Set1", "Set2", "Set3", "Set4", "Set5" });
+//
+//		// set custom labels and colors
+		l.setCustom(colorTemplate, new String[] { "Iron", "Fat", "Carb", "Prot", "Calc", "Fiber"});
 
 		chart.invalidate(); // refresh
 	}
